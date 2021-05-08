068049908b2e3cea76c7f01e563ea334787d3afa
何青 <qinghe@corp.netease.com>
2021-05-08 18:07:21 +0800

    add

---
+++ bingPic.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+from selenium import webdriver
+import time
+import re
+import requests
+import hashlib
+from datetime import datetime
+import os
+
+bingImgDir = 'bingImgs'
+driver = webdriver.Chrome(executable_path="/Users/heq/chromedriver")
+
+
+def download_img():
+    driver.get('https://cn.bing.com/')
+    try:
+        time.sleep(2)
+        element = driver.find_element_by_xpath('//*[@id="bgDiv"]')
+        url = str(element.value_of_css_property('background-image'))
+        # url("https://cn.bing.com/th?id=OHR.LimosaLimosa_ZH-CN8008396927_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4&r=0")
+        url = re.compile("url\(\"(.*)\"\)").findall(url)[0]
+        md5 = hashlib.md5(url.encode('utf-8')).hexdigest()
+        print('正在保存 md5 = %s url = %s ' % (md5, url))
+        image = requests.get(url).content
+        if not os.path.exists(bingImgDir):
+            os.mkdir(bingImgDir)
+        # with open(bingImgDir + '/bing_' + md5 + '.jpeg', 'wb') as fp:
+        with open(bingImgDir + '/bing_' + str(datetime.now().timestamp()) + '.jpeg', 'wb') as fp:
+            fp.write(image)
+    finally:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    download_img()
--- sendPush.py
+++ sendPush.py
@@ -2,9 +2,11 @@
 from selenium import webdriver
 import time
 # http://t.hz.c.m.163.com/admin/jidian/send?t=1619345435068&title=12345&content=67890&deviceIds=qqqq&payload=eweqew
+# hd http://t.hz.c.m.163.com/admin/login?openid.assoc_handle=%7BHMAC-SHA1%7D%7B5d301462%7D%7BasLLGQ%3D%3D%7D&openid.ax.mode=fetch_response&openid.claimed_id=https%3A%2F%2Flogin.netease.com%2Fopenid%2Fliupanwen%2F&openid.identity=https%3A%2F%2Flogin.netease.com%2Fopenid%2Fliupanwen%2F&openid.mode=id_res&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.ax=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.op_endpoint=https%3A%2F%2Flogin.netease.com%2Fopenid%2F&openid.response_nonce=2019-07-18T06%3A40%3A34ZBgfq5L&openid.return_to=http%3A%2F%2Ft.hz.c.m.163.com%2Fadmin%2Flogin&openid.sig=bQnnG3L6YlNPL04AyJ321nhk3vw%3D&openid.signed=assoc_handle%2Cax.mode%2Cclaimed_id%2Cidentity%2Cmode%2Cns%2Cns.ax%2Cns.sreg%2Cop_endpoint%2Cresponse_nonce%2Creturn_to%2Csigned%2Csreg.email%2Csreg.fullname%2Csreg.nickname&openid.sreg.email=liupanwen%40corp.netease.com&openid.sreg.fullname=%E5%88%98%E6%94%80%E6%96%87&openid.sreg.nickname=liupanwen#/tool/batchsendpush
 payload = {
     "showType": 0,
     "pushTime": "1606464024452",
+    
     "pushtype": "doc",
     "id": "FSBR44C10529HGFU",
     "stickyTimes": 2,
@@ -20,6 +22,7 @@ payload = {
     "boardid": ""
 }
 print(type(payload))
+# driver = webdriver.Chrome(executable_path="/Users/heq/chromedriver")  # 创建浏览器对象
 driver = webdriver.Chrome(executable_path="/Users/heq/chromedriver")  # 创建浏览器对象
 
 driver.get(
